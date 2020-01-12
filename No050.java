05abce870a0f3c61e651e119fb8d310bb4e97695
Xq-Yan <54343570+Xq-Yan@users.noreply.github.com>
2020-01-12 15:29:14 +1300

    Create No050.java

---
+++ No050.java
@@ -0,0 +1,50 @@
+/*
+50. Pow(x, n)
+
+Implement pow(x, n), which calculates x raised to the power n (xn).
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+Example 2:
+
+Input: 2.10000, 3
+Output: 9.26100
+Example 3:
+
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+Note:
+
+-100.0 < x < 100.0
+n is a 32-bit signed integer, within the range [−231, 231 − 1]
+*/
+
+class Solution {
+    public double myPow(double x, int n) {
+        double res = 1.0;
+        if (n == 0) return res;
+        if (n == Integer.MIN_VALUE && Math.abs(x) > 1) return 0;
+        if (n == Integer.MIN_VALUE && Math.abs(x) < 1) return Double.MAX_VALUE;
+        if (n == Integer.MIN_VALUE && Math.abs(x) == 1) return 1;
+        if (x < 0) {
+            if (n % 2 != 0) res = -1;
+            x = -x;
+        }
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        double last = 0;
+        if (n > 0) {
+            for (int i = 0; i < n; i++) {
+                res *= x;
+                if (res == last) return res;
+                last = res;
+            }
+        }
+        return res;
+    }
+}
