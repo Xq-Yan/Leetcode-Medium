a990e32a986b2be8aeb3672ff8105bd53b6b51f4
Xq-Yan <54343570+Xq-Yan@users.noreply.github.com>
2020-06-29 11:01:31 +1200

    Create No092.java

---
+++ No092.java
@@ -0,0 +1,39 @@
+/*
+92. Reverse Linked List II
+
+Reverse a linked list from position m to n. Do it in one-pass.
+
+Note: 1 ≤ m ≤ n ≤ length of list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL, m = 2, n = 4
+Output: 1->4->3->2->5->NULL
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode res = new ListNode(0);
+        res.next = head;
+        ListNode firstPart = res;
+        for (int i = 0; i < m- 1; i++) {
+            firstPart = firstPart.next;
+        }
+        head = firstPart.next;
+        for (int i = m; i < n; i++) {
+            ListNode curr = head.next;
+            head.next = curr.next;
+            curr.next = firstPart.next;
+            firstPart.next = curr;
+        }
+        return res.next;
+    }
+}
