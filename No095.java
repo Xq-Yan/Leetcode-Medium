3c9761de9a9ee0da20533c86f97dc359b7508fc0
Xq-Yan <54343570+Xq-Yan@users.noreply.github.com>
2020-06-30 16:42:19 +1200

    Create No095.java

---
+++ No095.java
@@ -0,0 +1,78 @@
+/*
+95. Unique Binary Search Trees II
+
+Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+
+Example:
+
+Input: 3
+Output:
+[
+  [1,null,3,2],
+  [3,2,null,1],
+  [3,1,null,null,2],
+  [2,1,3],
+  [1,null,2,null,3]
+]
+Explanation:
+The above output corresponds to the 5 unique BST's shown below:
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+ 
+
+Constraints:
+
+0 <= n <= 8
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) return new ArrayList<>();
+        return getAns(1, n);
+    }
+
+    private List<TreeNode> getAns(int start, int n) {
+        List<TreeNode> res = new ArrayList<>();
+        if (start > n) {
+            res.add(null);
+            return res;
+        }
+        if (start == n) {
+            TreeNode tree = new TreeNode(start);
+            res.add(tree);
+            return res;
+        }
+        for (int i = start; i <= n; i++) {
+            List<TreeNode> left = getAns(start, i - 1);
+            List<TreeNode> right = getAns(i + 1, n);
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+}
